ea092a1f4caffcef0f31ffe9b9cf723ddaf57f65
陈士男 <shinan.chen@hand-china.com>
2018-11-15 09:12:28 +0800

    [INIT] 初始化项目

---
+++ src/main/java/com/chenshinan/concurrent/CsnConcurrentApplication.java
@@ -0,0 +1,12 @@
+package com.chenshinan.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsnConcurrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsnConcurrentApplication.class, args);
+    }
+}
---
+++ src/test/java/com/chenshinan/concurrent/CsnConcurrentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chenshinan.concurrent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsnConcurrentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
